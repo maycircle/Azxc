0874bbcc371717790efa14a86f0c611e6c369069
Eugene <adcgame1@gmail.com>
2020-04-03 23:27:30 +0600

    Updated Window

--- build/src/UI/Controls/Window.cs
+++ build/src/UI/Controls/Window.cs
@@ -14,25 +14,40 @@ namespace Azxc.UI.Controls
         public Vec2 position { get; set; }
         public Vec2 size { get; set; }
 
-        private List<Control> _items;
+        // The position and size where all controls would start drawing
+        public Vec2 workPlacePosition;
+        public Vec2 workPlaceSize;
+
+        protected List<Control> _items;
+
+        // Not very important variable
+        public Vec2 indent;
 
         public Window(Vec2 position, Vec2 size)
         {
             this.position = position;
             this.size = size;
+            // This is only for pretty look, so i'll use standart value everywhere
+            indent = Vec2.One / 2;
 
             _items = new List<Control>();
         }
 
         public void Update()
         {
-
+            workPlacePosition = position + indent * 2;
+            workPlaceSize = size - indent * 2;
         }
 
         public override void Draw()
         {
             if (!visible)
                 return;
+
+            // Window borders themselves
+            Graphics.DrawRect(position, position + size, Color.Black);
+            Graphics.DrawRect(workPlacePosition - indent, workPlacePosition + workPlaceSize - indent,
+                Color.DarkSlateGray, 0.1f, false, 0.5f);
         }
 
         public virtual void Show()
