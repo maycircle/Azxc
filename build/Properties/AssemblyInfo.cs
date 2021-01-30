f8fdfc3505ba161849e8e50711496ca431f77331
Eugene Gritz <adcgame1@gmail.com>
2021-01-30 23:27:40 +0600

    Updated Console Implementation

--- build/Properties/AssemblyInfo.cs
+++ build/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.65")]
-[assembly: AssemblyFileVersion("1.2.0.65")]
+[assembly: AssemblyVersion("1.2.0.66")]
+[assembly: AssemblyFileVersion("1.2.0.66")]
--- build/src/DevConsoleVars.cs
+++ build/src/DevConsoleVars.cs
@@ -24,43 +24,13 @@ namespace Azxc
             }
         }
 
-        public static int ReadNextVariable(string s, ref int startIndex)
-        {
-            int index = startIndex;
-            if (TextParser.ReadNextBrace(s, ref index) == "{")
-            {
-                startIndex = index;
-                TextParser.ReadNextBrace(s, ref index);
-            }
-            if (index >= s.Length || s[startIndex] != '{')
-                return startIndex;
-            return index + 1;
-        }
-
-        public static List<string> GetAllVariables(string s)
-        {
-            List<string> variables = new List<string>();
-
-            int startIndex = 0, endIndex = 0;
-            while (endIndex + 1 < s.Length)
-            {
-                endIndex = ReadNextVariable(s, ref startIndex);
-                if (startIndex >= endIndex)
-                    break;
-                variables.Add(s.Substring(startIndex, endIndex - startIndex));
-                startIndex = endIndex;
-            }
-
-            return variables;
-        }
-
         static void Prefix(ref string command)
         {
             if (!enabled)
                 return;
 
             string oldCommand = command;
-            command = Regex.Replace(command, @"{([al]?)p(\d)}", delegate(Match match)
+            command = Regex.Replace(command, @"\[([al]?)p(\d)\]", delegate(Match match)
             {
                 List<Profile> profiles = Profiles.all.ToList();
                 if (match.Groups[1].Value == "a")
